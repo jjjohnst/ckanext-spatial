8704580fee163aca2b7540aaa85dd872609aedd3
fxia <fxia@reisystems.com>
2015-05-22 15:18:53 -0400

    return on error before new harvest object

--- ckanext/spatial/harvesters/base.py
+++ ckanext/spatial/harvesters/base.py
@@ -495,6 +495,12 @@ class SpatialHarvester(HarvesterBase):
                                     harvest_object, 'Import')
             return False
 
+        # Build the package dict
+        package_dict = self.get_package_dict(iso_values, harvest_object)
+        if not package_dict:
+            log.error('No package dict returned, aborting import for object {0}'.format(harvest_object.id))
+            return False
+
         # Flag previous object as not current anymore
         if previous_object and not self.force_import:
             previous_object.current = False
@@ -534,12 +540,6 @@ class SpatialHarvester(HarvesterBase):
 
         harvest_object.metadata_modified_date = metadata_modified_date
         harvest_object.add()
-
-        # Build the package dict
-        package_dict = self.get_package_dict(iso_values, harvest_object)
-        if not package_dict:
-            log.error('No package dict returned, aborting import for object {0}'.format(harvest_object.id))
-            return False
 
         # Create / update the package
 
